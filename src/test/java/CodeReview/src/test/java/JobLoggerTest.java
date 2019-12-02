c283662af62b0246adf97107f2e27ddaaabe0ba1
Jorge Saucedo <jorge.saucedo@protecso.com.pe>
2019-12-02 13:18:53 -0500

    Respuesta codeReview

--- src/test/java/CodeReview/src/test/java/JobLoggerTest.java
+++ src/test/java/CodeReview/src/test/java/JobLoggerTest.java
@@ -1,6 +1,5 @@
 package CodeReview.src.test.java;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.doThrow;
@@ -16,70 +15,72 @@ import junit.framework.Assert;
 
 @SpringBootTest
 public class JobLoggerTest {
-	
-    private JobLogger jobLogger;
+
+	private JobLogger jobLogger;
 	private String messageText = "Log Message";
-	
+
 	@Test
 	public void InvalidConfigurationTest() throws NoSuchFieldException, SecurityException, BusinessException {
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), false);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
-		doThrow(new BusinessException("Invalid configuration")).when(jobLogger).LogMessage(messageText, true, false, false);
-        Assert.assertTrue(true);
-     }
-	
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), false);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
+		doThrow(new BusinessException("Invalid configuration")).when(jobLogger).LogMessage(messageText, true, false,
+				false);
+		Assert.assertTrue(true);
+	}
+
 	@Test
 	public void ErrorMessageSpecifiedTest() throws NoSuchFieldException, SecurityException, BusinessException {
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), true);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
-		doThrow(new BusinessException("Error or Warning or Message must be specified")).when(jobLogger).LogMessage(messageText, true, false, false);
-        Assert.assertTrue(true);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), true);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
+		doThrow(new BusinessException("Error or Warning or Message must be specified")).when(jobLogger)
+				.LogMessage(messageText, true, false, false);
+		Assert.assertTrue(true);
 	}
-	
+
 	@Test
 	public void writeLogToConsoleTest() throws NoSuchFieldException, SecurityException, BusinessException {
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), true);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), true);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
 		jobLogger.LogMessage(messageText, true, false, false);
-        Assert.assertTrue(true);
-     }
-	
+		Assert.assertTrue(true);
+	}
+
 	@Test
 	public void writeLogToFileTest() throws NoSuchFieldException, SecurityException, BusinessException {
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), true);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), true);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
 		jobLogger.LogMessage(messageText, true, false, false);
-        Assert.assertTrue(true);
-     }
-	
+		Assert.assertTrue(true);
+	}
+
 	@Test
 	public void writeLogToDatabaseMessageTest() throws NoSuchFieldException, SecurityException, BusinessException {
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), true);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), true);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
 		jobLogger.LogMessage(messageText, true, false, false);
-        Assert.assertTrue(true);
-     }
-	
+		Assert.assertTrue(true);
+	}
+
 	@Test
 	public void writeLogToDatabaseWarningTest() throws NoSuchFieldException, SecurityException, BusinessException {
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), true);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), true);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
 		jobLogger.LogMessage(messageText, false, true, false);
-        Assert.assertTrue(true);
-     }
-	
+		Assert.assertTrue(true);
+	}
+
 	@Test
 	public void writeLogToDatabaseErrorTest() throws NoSuchFieldException, SecurityException, BusinessException {
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), true);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
-	    FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
-	    jobLogger.LogMessage(messageText, false, false, true);
-        Assert.assertTrue(true);
-     }
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToConsole"), true);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToFile"), false);
+		FieldSetter.setField(jobLogger, jobLogger.getClass().getDeclaredField("logToDatabase"), false);
+		jobLogger.LogMessage(messageText, false, false, true);
+		Assert.assertTrue(true);
+	}
 }
